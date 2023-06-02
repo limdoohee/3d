c90e318707b13d0b3db1ff48a38e3866a1ca0698
jskim <jskim@xxblue.com>
2023-06-02 12:58:24 +0900

    병합 오류 수정

--- pages/dds/tabs.js
+++ pages/dds/tabs.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Router, { useRouter } from "next/router";
 import React, { useState, useEffect, useRef, createRef, forwardRef } from "react";
 import { observer } from "mobx-react-lite";
-import "../_lib/module/i18n";
+import "../../_lib/module/i18n";
 
 import { useTranslation } from "react-i18next";
 //------------------------------------------------------------------------------- Store
@@ -16,8 +16,6 @@ import View from "../../_view/dds/tabs.view";
 let langSelect = "en";
 
 const Home = observer((props) => {
-<<<<<<< HEAD:_view/index_view.js
-    const {} = props;
     const router = useRouter();
 
     const { t, i18n } = useTranslation();
@@ -28,13 +26,7 @@ const Home = observer((props) => {
 
     return (
         <>
-            INDEX
-            {/* <div>{t(`description.part1`)}</div> */}
-=======
-    return (
-        <>
             <View props={props} />
->>>>>>> feature/antd:pages/dds/tabs.js
         </>
     );
 });
