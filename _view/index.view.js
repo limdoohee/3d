603ffcde3490e281189b28f2ca47543cd76bd8c8
jskim <jskim@xxblue.com>
2023-06-02 14:34:39 +0900

    buildId store 생성

--- _view/index.view.js
+++ _view/index.view.js
@@ -7,29 +7,19 @@ import DDS_Layout from "../_lib/component/layout";
 //------------------------------------------------------------------------------- Component
 
 const Home = observer((props) => {
-    const {} = props;
+    const { store } = props;
+    const { common } = store;
     const router = useRouter();
 
-    const [buildId, setBuildId] = React.useState(null);
-
-    React.useEffect(() => {
-        // const hasTargetFile = Array.from(document.querySelectorAll("script"))
-        //     .map(({ src }) => src)
-        //     .some((url) => url.includes("_buildManifest.js"));
-        // if (!hasTargetFile) {
-        //     return;
-        // }
-
-        setBuildId(JSON.parse(document.querySelector("#__NEXT_DATA__").textContent).buildId);
-
-        console.log(JSON.parse(document.querySelector("#__NEXT_DATA__").textContent).buildId);
+    React.useEffect(() => {  
+        common.getBuildId();
     }, []);
 
-    if (Boolean(buildId)) {
-        console.log("buildId: " + buildId);
-    }
-
-    return <DDS_Layout.container>INDEX</DDS_Layout.container>;
+    return (
+        <DDS_Layout.container>
+            INDEX <p>buildId : {common.buildId}</p>
+        </DDS_Layout.container>
+    );
 });
 
 export default Home;
