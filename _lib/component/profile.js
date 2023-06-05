386f3f7fff088fed3e4e1499ff89cb9a45243ee4
jskim <jskim@xxblue.com>
2023-06-05 10:51:04 +0900

    프로필 수정

--- _lib/component/profile.js
+++ _lib/component/profile.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect, useRef, createRef, forwardRef } from "react";
-import { Avatar, Badge } from "antd-mobile";
+import { Avatar, Badge } from "antd";
 import DDS_Icons from "../../_lib/component/icons";
 
 const Component = {
     default: (props) => {
         return (
             <div className={`dds profile ${props.className}`}>
-                <Avatar {...props}></Avatar>
-                <DDS_Icons.badgeCrown className="cert" />
+                {/* <Badge count={<DDS_Icons.badgeCrown className="cert" />}> */}
+                <Badge>
+                    <Avatar {...props}></Avatar>
+                </Badge>
             </div>
         );
     },
