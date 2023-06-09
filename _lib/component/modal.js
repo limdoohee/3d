5f0e77e2d512f5aca68fa1923c0b027996529a63
임두희 <limdh@xxblue.com>
2023-06-09 14:53:39 +0900

    [dds] modal, icon 추가

---
+++ _lib/component/modal.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { Drawer } from "antd";
+import DDS_Button from "./button";
+import Link from "next/link";
+
+const Component = (props) => {
+    const [open, setOpen] = useState(false);
+
+    const onClose = () => {
+        setOpen(false);
+    };
+
+    useEffect(() => {
+        setOpen(props.open);
+    }, [props]);
+
+    return (
+        <Drawer placement="bottom" closable={false} onClose={onClose} open={open} height="bottom" className="bottomModal">
+            {props.img && (
+                <div className="imgWrapper">
+                    <img src={props.img} />
+                </div>
+            )}
+            <h1 className="title">{props.title}</h1>
+            <h3 className="context">{props.context}</h3>
+            <DDS_Button.default className="dds button primary large">
+                <Link href={props.linkUrl}>{props.button}</Link>
+            </DDS_Button.default>
+            <DDS_Button.default className="dds button text" onClick={onClose}>
+                나중에
+            </DDS_Button.default>
+        </Drawer>
+    );
+};
+
+export default Component;
