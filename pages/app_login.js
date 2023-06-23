c6632292d996dfae7e454be9077cf3832e977e84
jskim <jskim@xxblue.com>
2023-06-23 16:39:49 +0900

    app login 페이지 추가

---
+++ pages/app_login.js
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import Router, { useRouter } from "next/router";
+import React, { useState, useEffect, useRef, createRef, forwardRef } from "react";
+import { observer } from "mobx-react-lite";
+import cookie, { plugToRequest } from "react-cookies";
+//------------------------------------------------------------------------------- Store
+import Store from "../_store/store";
+const store = new Store();
+//------------------------------------------------------------------------------- Store
+//------------------------------------------------------------------------------- Module
+import checkLogin from "../_lib/module/checkLogin";
+//------------------------------------------------------------------------------- Module
+//------------------------------------------------------------------------------- View
+import View from "../_view/index.view";
+//------------------------------------------------------------------------------- View
+
+const Home = observer((props) => {
+    common.debug(props);
+    useEffect(async () => {
+        await cookie.remove("loginToken", { path: "/" });
+        await cookie.save("loginToken", props.query.loginToken, { path: "/" });
+        location.href = "/";
+    }, []);
+    return <></>;
+});
+
+//------------------------------------------------------------------------------- getServerSideProps
+export async function getServerSideProps(context) {
+    let datas = { query: context.query };
+    return { props: datas };
+}
+//------------------------------------------------------------------------------- getServerSideProps
+
+export default Home;
