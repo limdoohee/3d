90f04a293b387f8343250670788f3f97033f3ed9
윤여진 <yjyoon@seoulauctionblue.com>
2023-06-29 11:41:58 +0000

    상용설정.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-echo 'deploy plum'
-ssh -i ~/.ssh/aws-front.pem ubuntu@3.38.186.137 ". ~/.nvm/nvm.sh && cd /srv/dropkitchen-front-plum && bash ./run-deploy.sh"
+echo 'deploy www'
+ssh -i ~/.ssh/aws-front.pem ubuntu@3.38.186.137 ". ~/.nvm/nvm.sh && cd /srv/dropkitchen-front-www && bash ./run-deploy.sh"
