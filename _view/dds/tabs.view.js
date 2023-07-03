cdb5c18048457005cac43d56aeddc81aa5bd38fc
임두희 <limdh@xxblue.com>
2023-07-03 11:34:43 +0900

    QA 수정

--- _view/dds/tabs.view.js
+++ _view/dds/tabs.view.js
@@ -14,7 +14,7 @@ const Home = observer((props) => {
     const onChange = (key) => {
         console.log(key);
     };
-    const items = [
+    const tabsGroup = [
         {
             key: "1",
             label: `메뉴1`,
@@ -47,6 +47,24 @@ const Home = observer((props) => {
         },
     ];
 
+    const tabs = [
+        {
+            key: "1",
+            label: `메뉴1`,
+            children: `Content of Tab Pane 1`,
+        },
+        {
+            key: "2",
+            label: `메뉴2`,
+            children: `Content of Tab Pane 2`,
+        },
+        {
+            key: "3",
+            label: `메뉴3`,
+            children: `Content of Tab Pane 3`,
+        },
+    ];
+
     return (
         <>
             <DDS.layout.container store={store}>
@@ -65,7 +83,14 @@ const Home = observer((props) => {
                     <p>Tabs는 페이지 내 유사한 정보를 그룹핑하여 콘텐츠를 정리할 때 사용합니다.</p>
                     <p>Tabs 와 Tabs group 2가지로 분류, Tabs 섹션이 2~4가지는 가장 기본적인 Tabs를 사용하며 섹션이 5가지 이상, 즉 다수일 경우 Tabs Group을 사용하며 길어질 경우 스크롤이 가능합니다.</p>
                     <div className="dds style-guide-inner">
-                        <DDS.tabs.default defaultActiveKey="1" items={items} onChange={onChange} />
+                        <div className="part">
+                            <DDS.tabs.default defaultActiveKey="0" items={tabsGroup} onChange={onChange} />
+                        </div>
+                        <br />
+                        <br />
+                        <div className="part">
+                            <DDS.tabs.default defaultActiveKey="1" items={tabs} onChange={onChange} />
+                        </div>
                     </div>
                 </div>
             </DDS.layout.container>
