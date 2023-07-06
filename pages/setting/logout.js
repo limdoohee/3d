c579300411fb4ef0db20db22f14201177e9987d4
jskim <jskim@xxblue.com>
2023-07-06 10:04:51 +0900

    로그아웃 API 콜 추가

--- pages/setting/logout.js
+++ pages/setting/logout.js
@@ -1,13 +1,20 @@
 import React, { useEffect } from "react";
 import cookie from "react-cookies";
+//------------------------------------------------------------------------------- Store
+import Store from "../../_store/store";
+const store = new Store();
+//------------------------------------------------------------------------------- Store
 
 const Home = () => {
+    const { auth } = store;
     useEffect(() => {
-        const logout = async (callback) => {
-            await cookie.remove("loginToken", { path: "/" });
-            await sessionStorage.clear();
-            // await localStorage.clear();
-            callback();
+        const logout = (callback) => {
+            auth.logout({}, async (e) => {
+                await cookie.remove("loginToken", { path: "/" });
+                await sessionStorage.clear();
+                // await localStorage.clear();
+                callback();
+            });
         };
         logout(() => {
             location.href = "/";
