47e206b32b1034688b332c00a94312f78fec6262
임두희 <limdh@xxblue.com>
2023-07-28 10:11:47 +0900

    개발 서버 ip 수정

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 echo 'deploy mango'
-ssh -i ~/.ssh/aws-front.pem ubuntu@3.38.186.137 ". ~/.nvm/nvm.sh && cd /srv/dropkitchen-front-mango && bash ./run-deploy.sh"
+ssh -i ~/.ssh/aws-front.pem ubuntu@3.35.228.254 ". ~/.nvm/nvm.sh && cd /srv/dropkitchen-front-mango && bash ./run-deploy.sh"
